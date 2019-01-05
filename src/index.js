f885bed151197b4f8384d133876d992c96a65917
dangvanduc90 <ducdangvan@vccorp.vn>
2019-01-06 01:02:07 +0700

    add apollo

--- src/index.js
+++ src/index.js
@@ -1,63 +1,24 @@
 var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const { ApolloServer, gql } = require('apollo-server-express');
+const GRAPHQL_PORT = 4000;
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const typeDefs = gql`
   type Query {
-    getMessage(id: ID!): Message
+    hello: String
   }
-  type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID!, input: MessageInput): Message
-  }
-  input MessageInput {
-    content: String
-    author: String
-  }
-  type Message {
-    id: ID!
-    content: String
-    author: String
-  }
-`);
-
-var fakeDatabase = {};
-// The root provides a resolver function for each API endpoint
-var root = {
-    getMessage: function ({id}) {
-        if (!fakeDatabase[id]) {
-            throw new Error("No record with id " + id);
-        }
-        return new Message(id, fakeDatabase[id]);
-    },
-    createMessage: function ({id, input}) {
-        var id = require('crypto').randomBytes(10).toString('hex');
-        fakeDatabase[id] = input;
-        return new Message(id, input);
-    },
-    updateMessage: function (id, input) {
-        if(!fakeDatabase[id]) {
-            throw new Error("No record with id " + id);
-        }
-        fakeDatabase[id] = input;
-        return new Message(id, input);
-    }
+`;
+ 
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
 };
+const server = new ApolloServer({ typeDefs, resolvers });
 
 var app = express();
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
+server.applyMiddleware({ app });
 
-class Message {
-    constructor(id, {content, author}) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-}
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+app.listen(GRAPHQL_PORT);
+console.log(`Running a GraphQL API server at localhost:${GRAPHQL_PORT}/graphql`);
