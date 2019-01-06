e8bf2da5c2c1a9ee392d153bf6a71f61644ce294
dangvanduc90 <ducdangvan@vccorp.vn>
2019-01-06 11:37:50 +0700

    sample node env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const GRAPHQL_PORT = 4000;
 
 const typeDefs = require('fs').readFileSync(__dirname + "/../schema/new_ticket_type.graphql", 'utf-8');
 const { resolvers } = require('./resolvers')
-
+const environment = process.env.NODE_ENV.trim() || 'development'
 const server = new ApolloServer({ typeDefs, resolvers });
 
 var app = express();
