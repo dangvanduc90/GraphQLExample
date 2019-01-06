1b882fdc1bda44204ebf683df4a663250254a4b4
dangvanduc90 <ducdangvan@vccorp.vn>
2019-01-06 15:37:51 +0700

    cache using dataloader

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -1,10 +1,58 @@
+const fetch = require('node-fetch');
+const DataLoader = require('dataloader');
+
 class BookClass {
-    getBook() {
-        return {
-            title: "How to",
-            author: "dangduc"
+    async getBook() {
+        // // return {
+        // //     title: "How to",
+        // //     author: "dangduc"
+        // // }
+        // const response = await fetch('https://api.github.com/users/github')
+        // const result = await response.json();
+        // // console.log(result)
+        // return {
+        //     title: result.bio,
+        //     author: result.email
+        // }
+
+
+
+        let result = await allGroupLoader.load('all_group').catch(error => {
+            if (error) {
+                allGroupLoader.clear('all_group');
+                console.log(error)
+            }
+            return null;
+        });
+        if(!result){
+            allGroupLoader.clear('all_group');
         }
+        return {
+            title: result.bio,
+            author: result.email
+        }
+        return result;
     }
 }
+var allGroupLoader = new DataLoader(keys => {
+    console.log(keys)
+    let result = new Promise(resolve => {
+        resolve(
+            keys.map(
+                // async key => {
+                //     let result = await fetch('https://api.github.com/users/github')
+                //     return await result.json();
+                // }
+                async key => {
+                    return {
+                        bio: "How to",
+                        email: "dangduc"
+                    }
+                }
+            )
+        )
+    });
+    return result;
+});
 const Book = new BookClass();
 export { Book }
